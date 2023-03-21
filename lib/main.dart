9b4e57e0aa7f5d7db55a7949188cc7286547fea9
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-22 00:05:55 +0530

    Text and Image Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  // const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('images/Profile_Image.jpeg'),
+                  radius: 50.0,
+                ),
+              ),
+              Text(
+                'Sachet Vijay',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'RobotoCondensed',
+                    fontSize: 30.0,
+                    color: Colors.white),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
